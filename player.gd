2861b45f4ffb40af0f7ce405285e7c166f036a34
Haydn <haydn.chapman@hotmail.com>
2023-07-24 22:31:15 +0100

    Heart Platformer - P1
    Added additional controller input.
    added animation speed proportional to player speed.

---
+++ player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+const ACCELERATION = 800.0
+const FRICTION = 800.0
+const JUMP_VELOCITY = -300.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("move_left", "move_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	update_animations(input_axis, velocity.x)
+	move_and_slide()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("jump") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+func update_animations(input_axis, x_velocity):	
+	if input_axis:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+		animated_sprite_2d.speed_scale = max(abs(x_velocity)/SPEED, 0.2)
+	else:
+		animated_sprite_2d.play("idle")
+
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
