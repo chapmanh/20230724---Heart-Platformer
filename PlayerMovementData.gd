4bb5a255014e54b6307675b0164c4a5ac364a332
Haydn <haydn.chapman@hotmail.com>
2023-07-26 23:50:10 +0100

    Double jump and Wall jump
    Managed to solve the issues of wall jump consuming double jump and not wall jumping when in a corner independently, so code starting to differ from original. Added wall slide speed to give a grippy feel to walls.

--- PlayerMovementData.gd
+++ PlayerMovementData.gd
@@ -2,8 +2,10 @@ class_name PlayerMovementData
 extends Resource
 
 @export var speed = 100.0
+@export var wall_slide_speed = 50.0
 @export var acceleration = 800.0
 @export var friction = 800.0
-@export var air_resistance = 350.0
+@export var air_resistance = 200
 @export var jump_velocity = -300.0
 @export var gravity_scale = 1.0
+@export var air_acceleration = 400.0
--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var movement_data : PlayerMovementData
 
+var air_jump = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -10,8 +11,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
+	handle_walljump()
 	var input_axis = Input.get_axis("move_left", "move_right")
 	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animations(input_axis, velocity.x)
 	var was_on_floor = is_on_floor()
@@ -19,23 +22,45 @@ func _physics_process(delta):
 	var just_left_ledge = (was_on_floor != is_on_floor()) and velocity.y >=0
 	if just_left_ledge:
 		coyote_jump_timer.start()
-	
-	
+
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta 
 
+func handle_walljump():
+	if not is_on_wall():
+		return
+	velocity.y = min(velocity.y, movement_data.wall_slide_speed)
+	
+	var wall_normal = get_wall_normal()
+	if not is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+		
 func handle_jump():
+	if is_on_floor():
+		air_jump = true
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = movement_data.jump_velocity
-	elif !is_on_floor():
+	elif !is_on_floor() and !is_on_wall():
 		if Input.is_action_just_released("jump") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
+		if Input.is_action_just_pressed("jump") and air_jump:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
 
 func handle_acceleration(input_axis, delta):
+	if not is_on_floor():
+		return
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor():
+		return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
 
 func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
